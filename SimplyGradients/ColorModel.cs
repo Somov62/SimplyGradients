6f31529d5e34c77604d31a09b42ce10d4982810f
olimp <olimp@LAB09-04>
2023-01-12 14:39:58 +0300

    Добавьте файлы проекта.

---
+++ SimplyGradients/ColorModel.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using System.Windows.Media;
+
+namespace SimplyGradients
+{
+    public class ColorModel : INotifyPropertyChanged
+    {
+
+        public ColorModel()
+        {
+            A = 255;
+        }
+        public Color SolidColor { get; set; }
+
+
+        private byte _a;
+        public byte A
+        {
+            get => _a;
+            set
+            {
+                _a = value;
+                ToBrush();
+            }
+        }
+
+        private byte _r;
+        public byte R
+        {
+            get => _r;
+            set
+            {
+                _r = value;
+                ToBrush();
+            }
+        }
+
+        private byte _g;
+        public byte G
+        {
+            get => _g;
+            set
+            {
+                _g = value;
+                ToBrush();
+            }
+        }
+
+        private byte _b;
+        public byte B
+        {
+            get => _b;
+            set
+            {
+                _b = value;
+                ToBrush();
+            }
+        }
+
+        private void ToBrush()
+        {
+            SolidColor = Color.FromArgb(A, R, G, B);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("SolidColor"));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+    }
+}
---
+++ SimplyGradients/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SimplyGradients
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public ColorModel SliderLeftColor { get; set; } = new ColorModel();
+        public ColorModel SliderRightColor { get; set; } = new ColorModel();
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            double percent = e.NewValue / 100;
+
+            var col1 = SliderLeftColor;
+            var col2 = SliderRightColor;
+
+            byte a = (byte)(col1.A + percent * (col2.A - col1.A));
+            byte r = (byte)(col1.R + percent * (col2.R - col1.R));
+            byte g = (byte)(col1.G + percent * (col2.G - col1.G));
+            byte b = (byte)(col1.B + percent * (col2.B - col1.B));
+            selectedColorPresenter.Background = new SolidColorBrush(Color.FromArgb(a, r, g, b));
+        }
+
+    }
+}
